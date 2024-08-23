7109e8d8d9839b8d3e853821afec9c1b76fbb5f3
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-08-23 08:39:57 -0400

    Commit

---
+++ UserInput.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+print("The user's name is : ", name)
+Droids = input("Number of Droids: ")
+print("Number of Droids Stored : ", Droids)
+Wookiees = input("Number of Wookiees: ")
+print("Number of Wookiees Stored : ", Wookiees)
+print(name, "wants to meet", Droids, "Droids and", Wookiees, "Wookiees!")
